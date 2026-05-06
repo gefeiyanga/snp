70ef526c576d632b0137c8e63f8f9829b1c0e682
variousdid <variousdid@gmail.com>
2026-05-07 01:22:40 +0800

    Polish global toast styling

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
+import { setToastDefaultOptions } from 'vant'
+import 'vant/es/toast/style'
 import App from './App.vue'
 import router from './router'
 import './styles/global.less'
+
+setToastDefaultOptions({
+  position: 'top',
+  className: 'finance-toast'
+})
 
 const app = createApp(App)
 
